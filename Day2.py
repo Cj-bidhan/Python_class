ce3c0b2d9b5e10a025c9f634b7d18b2b1ad99086
Bidhan Ghimire <bidhang99@gmail.com>
2024-01-10 10:40:24 +0545

    added a new file

---
+++ Day2.py
@@ -0,0 +1,7 @@
+## Assignment - String, list , tuple, set, Dictionary. (Methods)
+## Learn how to reverse, sort, index, slicing, remove
+## Ask 2 number input from users, and perform mathematical calculations.
+## also power and remainder.
+##Check whats new in python-theory (3.8-1.12)
+
+
