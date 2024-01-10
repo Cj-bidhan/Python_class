d088d3c4638bd990b6a6bcb5c61729c536b4604c
Bidhan Ghimire <bidhang99@gmail.com>
2024-01-10 10:42:17 +0545

    Added new folder

---
+++ class-studies/calc_of_SI.py
@@ -0,0 +1,14 @@
+## Assignment - String, list , tuple, set, Dictionary. (Methods)
+## Learn how to reverse, sort, index, slicing, remove
+## Ask 2 number input from users, and perform mathematical calculations.
+## also power and remainder.
+##Check whats new in python-theory (3.8-1.12)
+
+
+#demonstration of float number, calculation of simple interest.
+principle  = float(input("Enter the principle amount:  "))
+rate  = float(input("Enter the rate of the interest:  "))
+time = float(input("Enter the duration of time period:  "))
+si = (principle * time * rate)/ 100
+print ("the simple intertest is: ", si)
+ 
