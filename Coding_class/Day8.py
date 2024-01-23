3fdf7e12dac5249825d9c221c282ef0fc4ecf539
Bidhan Ghimire <bidhang99@gmail.com>
2024-01-23 18:27:12 +0545

    Updated files and folders

---
+++ Coding_class/Day8.py
@@ -0,0 +1,7 @@
+
+def generate_cube (a):
+    return a**3
+a = generate_cube(2)
+print ("The cube of given number is ", a)
+    
+    
---
+++ Coding_class/Day9.py
@@ -0,0 +1,10 @@
+#modules
+# import datetime #built in modules
+# import os
+# import sys
+
+# from function import add #custom module
+
+
+
+from Day8 import generate_cube
